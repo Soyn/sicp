3d28ec2f8f9fa400fbc0cef72b2a024f154358ab
Soyn <w604408482@163.com>
2015-05-16 17:46:41 +0800

    2.1

---
+++ EX2.1.rkt
@@ -0,0 +1,16 @@
+#lang racket
+;exercise 2.1
+;author:gcMerge
+
+(define (number x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (number x))
+  (display "/")
+  (display (denom x)))
+
+(define (make-rat n d)
+  (let ((g((if(< d 0)- +)(gcd n d))))
+    (cons(/ n g)(/ d g))))
