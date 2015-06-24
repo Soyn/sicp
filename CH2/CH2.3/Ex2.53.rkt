baeedefb0865d6c363ebe19cf0014d5980524053
Soyn <w604408482@163.com>
2015-06-24 22:20:43 +0800

    Add CH2.3

---
+++ CH2/CH2.3/Ex2.53.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+;Ex 2.53
+
+(display "(list 'a 'b 'c):\n" )
+(list 'a 'b 'c)
+
+(display "(list (list 'george)):\n")
+
+( list ( list 'george))
+
+(display " (cdr'((x1 x2) (y1 y2))):\n")
+( cdr '( ( x1 x2) ( y1 y2)))
+
+(display "(cadr ( (x1 x2) ( y1y2))):\n")
+( cadr ' ( (x1 x2) ( y1 y2)))
+
+(display "(pair? (car '(a short list))):\n")
+( pair? ( car '( a short list)))
+
+(display " (memq 'red '( ( red shoes) ( blue socks))):\n")
+( memq 'red '( ( red shoes) ( blue socks)))
+
+( display " (memq 'red '(red shoes blue socks)):\n")
+(memq 'red '( red shoes blue socks))
