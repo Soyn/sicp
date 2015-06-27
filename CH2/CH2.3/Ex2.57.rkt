ae4afa774c27414cbab5a6c269427e8eea90a176
Soyn <w604408482@163.com>
2015-06-27 21:02:49 +0800

    Ex2.57

---
+++ CH2/CH2.3/Ex2.57.rkt
@@ -0,0 +1,84 @@
+#lang racket
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (variable? e)
+  (symbol? e))
+
+(define (same-variable v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (sum? e)
+  (and (pair? e) (eq? (car e) '+)))
+
+(define (addend e)
+  (cadr e))
+
+(define (augend e)
+  (foldr make-sum 0 (cddr e)))
+            
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (product? e)
+  (and (pair? e) (eq? (car e) '*)))
+
+(define (multiplier e)
+  (cadr e))
+
+(define (multiplicand e)
+  (foldr make-sum 0 (cddr e)))
+
+(define (exponentiation? e)
+  (and (pair? e) (eq? (car e) '**)))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
+
+(define (make-exponentiation b e)
+  (cond ((=number? b 0) 0)
+        ((=number? e 1) b)
+        ((=number? e 0) 1)
+        ((and (number? b) (number? e)) (expt b e))
+        (else (list '** b e))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+;; Derivative
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum (make-product (multiplier exp)
+                                 (deriv (multiplicand exp) var))
+                   (make-product (deriv (multiplier exp) var)
+                                 (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (exponent exp)
+                       (make-product (make-exponentiation (base exp)
+                                                          (make-sum (exponent exp) -1))
+                                     (deriv (base exp) var))))
+        (else
+         (error "Unknown expression type " exp))))
+
+
+
+
+(deriv '(* x x (+ x 3)) 'x)
+(deriv '(+ x x (+ x 3)) 'x)
