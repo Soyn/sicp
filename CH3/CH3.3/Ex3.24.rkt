2a2c728324cfee1e12e1efe4f6652496ff529081
Soyn <w604408482@163.com>
2015-09-22 22:38:13 +0800

    add Ex3.24

---
+++ CH3/CH3.3/Ex3.24.rkt
@@ -0,0 +1,44 @@
+#lang planet neil/sicp
+;;
+;; Exercise3.24
+;; Created by Soyn. 22/9/15
+;; @Brief:In the table implementations above, the keys are tested for equality using equal? (called
+;; by assoc). This is not always the appropriate test. For instance, we might have a table with numeric keys
+;; in which we don't need an exact match to the number we're looking up, but only a number within some
+;; tolerance of it. Design a table constructor make-table that takes as an argument a same-key?
+;; procedure that will be used to test ``equality'' of keys. Make-table should return a dispatch
+;; procedure that can be used to access appropriate lookup and insert! procedures for a local table.
+;;
+
+( define ( make-table same-key?)                ;; all we need to change the 'assoc' procedure
+   ( let ( ( local-table (list '*table*)))
+      ( define ( assoc key records)
+         ( cond ( ( null? records) false)
+               (( same-key? key ( caar records)) ( car records))
+               ( else ( assoc key ( cdr records)))))
+      ( define ( lookup key-1 key-2)
+         ( let ( ( subtable ( assoc key-1 ( cdr local-table))))
+            ( if subtable
+                ( let ( ( record ( assoc key-2 ( cdr subtable))))
+                   ( if record
+                       ( cdr record)
+                       false))
+                false)))
+            ( define ( insert! key-1 key-2 value)
+               ( let ( ( subtable ( assoc key-1 ( cdr local-table))))
+                  ( if subtable
+                      ( let ( ( record ( assoc key-2 ( cdr subtable))))
+                         ( if record
+                             ( set-cdr! record value)
+                             ( set-cdr! subtable
+                                       ( cons ( cons key-2 value)
+                                             ( cdr subtable)))))
+                      ( set-cdr! local-table
+                                ( cons ( list key-1
+                                             ( cons key-2 value))
+                                      ( cdr local-table))))))
+            ( define ( dispatch m)
+               ( cond ( ( eq? m 'lookup-proc) lookup)
+                     ( ( eq? m 'insert-proc!) insert!)
+                     ( else ( error "Unknown operation -- TABLE"m))))
+            dispatch))
